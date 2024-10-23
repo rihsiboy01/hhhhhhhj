ac7a330730eb9e84983e2280e45e216c1b996ff9
rihsiboy01 <auzcom78@gmail.com>
2024-10-23 15:26:32 +0300

    Add files via upload

---
+++ skripr.js
@@ -0,0 +1,12 @@
+let a = prompt ('salom')
+let c = prompt ('mening ismim ')
+let g = prompt ('rihsiboy')
+let h = alert ('rahmat')
+console.log(a,c,g);
+console.log(c);
+console.log(g);
+console.log(h);
+
+
+
+
